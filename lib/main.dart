e7d51de6d68452f6cf7d3ab71bf76f9f39e5b60a
HarshHirapara <harsh.h@joflee.com>
2023-12-25 15:26:10 +0530

    [implement] change clander picker and close keybord

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,16 @@ import 'package:get/get.dart';
 import 'module/screen/from_main_page_screen/main_form_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      // Status bar color
+      statusBarColor: Colors.red,
+
+      // Status bar brightness (optional)
+      statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+      statusBarBrightness: Brightness.light, // For iOS (dark icons)
+    ),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
     [
@@ -11,6 +21,7 @@ void main() {
       DeviceOrientation.portraitDown,
     ],
   );
+
   runApp(
     const GetMaterialApp(
       debugShowCheckedModeBanner: false,
